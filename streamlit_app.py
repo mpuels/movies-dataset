8b709f6dc412d776751abf6e47758538ce29457f
Marc Püls <mpuels@gmail.com>
2025-12-01 18:22:53 +0100

    Update title to reflect app ownership

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 # Show the page title and description.
 st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset from TMDB")
+st.title("🎬 Movies dataset from TMDB - copied by Marc")
 st.write(
     """
     This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
